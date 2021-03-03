6dfa90818eaee98d89a2b48b49ebb5787cc1669a
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2021-03-03 18:17:50 -0300

    Importados paquetes UI-Switch y Archwizard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
+/* Modules */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UiSwitchModule } from 'ngx-ui-switch';
+import { ArchwizardModule } from 'angular-archwizard';
 
+/* Components */
 import { AppComponent } from './app.component';
 import { MainComponent } from './layout/main/main.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { LayoutComponent } from './layout/layout.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +22,11 @@ import { LayoutComponent } from './layout/layout.component';
     LayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    UiSwitchModule,
+    ArchwizardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layout/main/main.component.ts
+++ src/app/layout/main/main.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  enable = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
